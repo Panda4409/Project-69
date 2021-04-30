e34efef2526a36173b22ec891f911b2c4119ee2a
Nishka <68439163+Panda4409@users.noreply.github.com>
2021-04-29 20:01:42 -0400

    Add files via upload

---
+++ Bar_Code_Scanner/screens/ScanScreen.js
@@ -0,0 +1,23 @@
+export default class ScanScreen extends React.component{
+    constructor(){
+        super();
+        this.state = {
+            hasCameraPermision: null,
+            scanned: false,
+            scannedData: '',
+            buttonState: 'normal'
+        }
+    }
+}
+
+getCameraPermision(){
+
+}
+
+handleBarCodeScanner(){
+
+}
+
+if (button == 'clicked'){
+    buttonState = 'clicked'
+}
